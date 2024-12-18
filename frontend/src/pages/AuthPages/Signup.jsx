1f8a023d741d7a43791a61d47af750335f3e34e2
abitshrestha03 <110910933+abitshrestha03@users.noreply.github.com>
2024-12-18 13:00:38 +0545

    Update Signup.jsx

--- frontend/src/pages/AuthPages/Signup.jsx
+++ frontend/src/pages/AuthPages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
+import { NavLink, useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 import axios from "axios";
+import NavLogo from "../../assets/images/NavLogo.svg";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 const SignupPage = () => {
@@ -52,9 +53,12 @@ const SignupPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="max-w-md w-full bg-white shadow-md rounded-lg p-6">
-        <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">
+        {/* <h2 className="text-2xl font-bold text-gray-800 text-center">
           Sign Up
-        </h2>
+        </h2> */}
+        <div className="flex justify-center mb-6">
+          <img src={NavLogo} alt="Logo" className="w-[15vw] h-auto" />
+        </div>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
@@ -70,7 +74,6 @@ const SignupPage = () => {
               className="w-full px-4 py-2 border rounded-lg text-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Username . . ."
               onChange={handleChange}
-              required
             />
           </div>
           <div className="mb-4">
@@ -87,7 +90,6 @@ const SignupPage = () => {
               className="w-full px-4 py-2 border rounded-lg text-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="you@example.com"
               onChange={handleChange}
-              required
             />
           </div>
           <div className="mb-4">
@@ -104,7 +106,6 @@ const SignupPage = () => {
               className="w-full px-4 py-2 border rounded-lg text-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Enter your password"
               onChange={handleChange}
-              required
             />
           </div>
           <div className="mb-4">
@@ -121,7 +122,6 @@ const SignupPage = () => {
               className="w-full px-4 py-2 border rounded-lg text-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Confirm your password"
               onChange={handleChange}
-              required
             />
           </div>
           <button
@@ -133,9 +133,9 @@ const SignupPage = () => {
         </form>
         <p className="text-sm text-gray-600 text-center mt-4">
           Already have an account?{" "}
-          <a href="#" className="text-indigo-500 hover:underline">
+          <NavLink to="/signin" className="text-indigo-500 hover:underline">
             Log In
-          </a>
+          </NavLink>
         </p>
       </div>
     </div>
@@ -143,3 +143,4 @@ const SignupPage = () => {
 };
 
 export default SignupPage;
+
